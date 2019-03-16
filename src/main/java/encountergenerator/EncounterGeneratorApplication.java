06baa4ac6f422cb56255b3025bd8a4a70df1257e
Kyle Murphy <kcmurphy82@gmail.com>
2019-03-16 14:16:16 -0500

    rebuild with spring 1.5.19 due to build issues

---
+++ src/main/java/encountergenerator/EncounterGeneratorApplication.java
@@ -0,0 +1,13 @@
+package encountergenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncounterGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncounterGeneratorApplication.class, args);
+	}
+
+}
