febb6adf1c54146e6ca2be43f7e77d747da7edc9
Kyle Murphy <kcmurphy82@gmail.com>
2019-04-04 20:33:21 -0500

    inital login work

--- src/main/java/encountergenerator/controllers/LoginController.java
+++ src/main/java/encountergenerator/controllers/LoginController.java
@@ -1,21 +1,46 @@
 package encountergenerator.controllers;
 
+import encountergenerator.models.forms.Users;
+import encountergenerator.models.data.LoginDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
 
 @Controller
 @RequestMapping(value = "login")
 public class LoginController {
 
     @Autowired
-    private LoginController LoginDao;
+    private LoginDao loginDao;
 
     @RequestMapping(value = "")
-    public String login(Model model){
+        public String login(Model model){
+        model.addAttribute(new Users());
         model.addAttribute("title", "Login");
         return "/login/index";
+    }
+
+    @RequestMapping(value="register", method = RequestMethod.GET)
+    public String register(Model model){
+        model.addAttribute(new Users());
+        model.addAttribute("title", "Register");
+        return "/login/register";
+    }
+
+    @RequestMapping(value = "register", method = RequestMethod.POST)
+    public String processaddUser(Model model, @ModelAttribute @Valid Users users, Errors errors){
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Register");
+            return "creatures/add";
+        }
+        loginDao.save(users);
+        return "redirect:";
     }
 }
--- src/main/java/encountergenerator/models/forms/Users.java
+++ src/main/java/encountergenerator/models/forms/Users.java
@@ -1,8 +1,12 @@
 package encountergenerator.models.forms;
 
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.annotation.CreatedDate;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Date;
 
 
 @Entity
@@ -12,12 +16,22 @@ public class Users {
     private Integer id;
 
     @NotNull
+    @NotBlank
     @Size(min=3, max=15)
     private String name;
-    private String password;
-    private String cratedate;
 
-    public Users(String name, String password, String cratedate) {
+    @NotNull
+    @NotBlank
+    private String password;
+
+    @NotNull
+    @NotBlank
+    private String verify;
+
+    @CreatedDate
+    private Date cratedate;
+
+    public Users(String name, String password, Date cratedate) {
         this.name = name;
         this.password = password;
         this.cratedate = cratedate;
@@ -47,7 +61,11 @@ public class Users {
         this.password = password;
     }
 
-    public String getCratedate() {
+    public String getVerify() { return verify; }
+
+    public void setVerify(String verify) { this.verify = verify; }
+
+    public Date getCratedate() {
         return cratedate;
     }
 
